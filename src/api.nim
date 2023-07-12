ac5d7cadb88730eff62e394f665b359e95cbe40a
hamidb80 <hr.bolouri@gmail.com>
2023-07-12 10:35:14 +0430

    pretty

--- src/api.nim
+++ src/api.nim
@@ -2,15 +2,8 @@ import std/[strutils, json, nre, htmlparser, random]
 import client, std/httpclient
 import utils
 
-const
-  baseUrl* = "https://food.shahed.ac.ir"
-  userPage* = "https://food.shahed.ac.ir/#!/UserIndex"
-  capchaUrlRaw = "https://food.shahed.ac.ir/api/v0/Captcha?id="
 
-
-proc freshCapchaUrl*: string =
-  capchaUrlRaw & $(rand 1..1000000)
-
+# ----- utils -----
 
 func repl(match: RegexMatch): string =
   match.captures[0].entityToUtf8
@@ -18,6 +11,7 @@ func repl(match: RegexMatch): string =
 func toHumanReadable(s: string): string =
   s.replace(re"&(\w+);", repl)
 
+# ----- working with data objects -----
 
 proc extractLoginPageData*(htmlPage: string): JsonNode =
   const
@@ -32,13 +26,11 @@ proc extractLoginPageData*(htmlPage: string): JsonNode =
   .toHumanReadable
   .parseJson
 
-
 func extractLoginUrl*(loginPageData: JsonNode): string =
   baseUrl & loginPageData["loginUrl"].getStr
 
 func extractLoginXsrfToken*(loginPageData: JsonNode): string =
   getStr loginPageData{"antiForgery", "value"}
-
 
 func loginForm*(user, pass, captcha, token: string): auto =
   {
@@ -47,11 +39,10 @@ func loginForm*(user, pass, captcha, token: string): auto =
     "Captcha": captcha,
     "idsrv.xsrf": token}
 
-
 func cleanLoginCapcha*(binary: string): string =
   binary.truncOn jfifTail
 
-# ----------------
+# ----- meta programming -----
 
 template convertFn(t: type bool): untyped = toBool
 template convertFn(t: type int): untyped = parseInt
@@ -62,5 +53,15 @@ template staticApi(name, typecast, url): untyped =
   proc name*(c: CustomHttpClient): typecast =
     convertFn(typecast)(c.httpc.getcontent url)
 
+# ----- API -----
+
 staticApi isCapchaEnabled, bool, "https://food.shahed.ac.ir/api/v0/Captcha?isactive=wehavecaptcha"
 staticApi credit, int, "https://food.shahed.ac.ir/api/v0/Credit"
+
+const
+  baseUrl* = "https://food.shahed.ac.ir"
+  userPage* = "https://food.shahed.ac.ir/#!/UserIndex"
+  capchaUrlRaw = "https://food.shahed.ac.ir/api/v0/Captcha?id="
+
+proc freshCapchaUrl*: string =
+  capchaUrlRaw & $(rand 1..1000000)
