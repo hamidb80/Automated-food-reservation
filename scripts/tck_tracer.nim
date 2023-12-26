119cb073730f4788a1503ce077d3487c14277b71
hamidb80 <hr.bolouri@gmail.com>
2023-12-26 22:17:33 +0330

    w

--- scripts/tck_tracer.nim
+++ scripts/tck_tracer.nim
@@ -7,15 +7,21 @@ let j = parseJson readfile "eduportal.shahed.ac.ir.har"
 for e in j{"log", "entries"}:
   let url = getStr e{"request", "url"}
 
-  if "POST" == getStr e{"request", "method"}:
+
+  echo "-------------"
+  echo url["https://eduportal.shahed.ac.ir/".len .. ^1]
+  
+  case getStr e{"request", "method"}
+  of "POST":
     let
       reqt = e{"request", "postData", "text"}
       rest = e{"response", "content", "text"}
       reqd = parseJson getStr reqt
       resd = parseJson getStr rest
 
-    echo "-------------"
-    echo url.substr(len "https://eduportal.shahed.ac.ir/frm")
     echo ">> ", getStr reqd{"aut", "tck"}
     echo "<< ", getStr resd{"aut", "tck"}
     echo "<< ", getStr resd{"oaut", "oa", "nmtck"}
+  
+  of "GET":
+    discard
