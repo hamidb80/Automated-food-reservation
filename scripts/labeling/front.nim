59fda82624fe9a9daf2428586f0d7c63d3ed78a2
hamidb80 <hr.bolouri@gmail.com>
2024-02-20 12:00:32 +0330

    clean

--- scripts/labeling/front.nim
+++ scripts/labeling/front.nim
@@ -1,38 +1,56 @@
-import std/[dom, jsfetch, asyncjs, sugar]
+import std/[
+  dom,
+  jsfetch,
+  asyncjs,
+  sugar]
+
 include karax/prelude
 
+## --- global states
+
 var currentImage = cstring""
+
+## --- actions
 
 proc updateImage(s: cstring) =
   currentImage = s
   redraw()
 
-proc nextImage = 
+proc nextImage =
   discard fetch(cstring "/image/next")
   .then((r: Response) => r.text())
   .then((t: cstring) => updateImage t)
 
+proc saveImage(value: cstring) =
+  discard fetch("/save/" & currentImage & "/" & value & "/")
+
+## --- views
 
 proc createDom: VNode =
   result = buildHtml main:
     tdiv(class = "frame"):
       img(src = "/image/" & currentImage)
+
     form(class = "user"):
-      input(placeholder = "captcha", id = "x")
+      input(placeholder = "enter captcha", id = "x")
       button:
         text "next"
 
       proc onsubmit(e: Event; n: VNode) =
-        preventDefault e 
-        let el = document.getElementById("x")
-        let val = el.value
+        preventDefault e
+        let
+          el = document.getElementById("x")
+          val = el.value
         el.value = ""
         el.focus
-        discard fetch("/save/" & currentImage & "/" & val & "/")
+        saveImage val
         nextImage()
-        
-proc start =
-  nextImage()
-  setRenderer createDom
 
-discard settimeout(start, 1000)
+## --- setup
+
+when isMainModule:
+  proc start =
+    nextImage()
+    setRenderer createDom
+
+  discard settimeout(start, 1000)
