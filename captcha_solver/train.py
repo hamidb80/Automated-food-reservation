4e1a4e75d2145e9e0d03d74570e07b1cd60b025d
hamidb80 <hr.bolouri@gmail.com>
2024-02-21 13:40:15 +0330

    add filter

--- captcha_solver/train.py
+++ captcha_solver/train.py
@@ -25,12 +25,12 @@ class ModelConfigs(BaseModelConfigs):
         super().__init__()
         self.model_path = "models/"
         self.vocab = None # assigned later
-        self.height = 50
+        self.height = 44 # 50
         self.width = 140
         self.max_text_length = 0
         self.batch_size = 64
         self.learning_rate = 1e-3
-        self.train_epochs = 100
+        self.train_epochs = 1000
         self.train_workers = 20
         self.split_data_ratio = 0.9
     
