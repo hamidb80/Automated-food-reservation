4e2593c36cf95d2095f3d92494253cdd6796f652
hamidb80 <hr.bolouri@gmail.com>
2024-02-23 14:49:16 +0330

    clean

--- scripts/dsampl_capchas.nim
+++ scripts/dsampl_capchas.nim
@@ -2,35 +2,32 @@ import std/[strformat, strutils, httpclient, os, random]
 
 randomize()
 
+
 proc newCaptchaUrl: string = 
   let n = rand 0..100
   "https://eduportal.shahed.ac.ir/frm/captcha/captcha.ashx?rr=1&x" & $n
 
-proc batchDownload(limit: Slice[int]) =
+proc startDownload(dest: string, bound: Slice[int]) =
   let c = newHttpClient()
-  var
-    lastImageLen = 0
-    i = limit.a
+  var i = bound.a
 
-  while i in limit:
-    sleep 100
+  while i in bound:
     try:
       let image = c.getContent newCaptchaUrl()
-      if lastImageLen != len image:
-        writeFile fmt"./temp/c-{i:05}.gif", image
-        echo i
-        inc i
-        lastImageLen = len image
+      writeFile dest / fmt"c-{i:05}.gif", image
+      echo i
+      inc i
 
     except:
       echo "Error: ", getCurrentExceptionMsg()
       
 when isMainModule:
-  if paramCount() != 2:
-    quit "USAGE: app <start-number> <end-number>"
+  if paramCount() != 3:
+    quit "USAGE: app <dest-dir> <start-number> <end-number>"
   else:
     let
-      a = parseInt paramStr 1
-      b = parseInt paramStr 2
+      dest = paramStr 1
+      a = parseInt paramStr 2
+      b = parseInt paramStr 3
 
-    batchDownload a .. b
+    startDownload dest, a .. b
